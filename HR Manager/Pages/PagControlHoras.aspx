3fa424b8b247200c16602e3ccd0070f815913628
naz0723 <aleston23.naza@gmail.com>
2024-11-18 15:06:13 -0600

    Clase,formulario Control de horas

---
+++ HR Manager/Pages/PagControlHoras.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PagControlHoras.aspx.cs" Inherits="HR_Manager.Pages.PagControlHoras" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Gestión de Control de Horas</title>
+<link type="text/css" rel="stylesheet" href="../Styles/Estilos.css" />
+</head>
+<body>
+    <form id="formControlHoras" runat="server">
+        <div class="container">
+            <h2>Gestión de Control de Horas</h2>
+
+            <!-- Formulario para agregar un control de horas -->
+            <div class="form-section">
+                <h3>Agregar Control de Horas</h3>
+                <label for="txtEmpleadoID">ID del Empleado:</label>
+                <input type="number" id="txtEmpleadoID" runat="server" />
+
+                <label for="txtFecha">Fecha:</label>
+                <input type="date" id="txtFecha" runat="server" />
+
+                <label for="txtHoraEntrada">Hora de Entrada:</label>
+                <input type="time" id="txtHoraEntrada" runat="server" />
+
+                <asp:Button ID="btnAgregarControlHoras" runat="server" Text="Agregar Control de Horas" OnClick="btnAgregarControlHoras_Click" />
+            </div>
+
+            <!-- Formulario para actualizar un control de horas -->
+            <div class="form-section">
+                <h3>Actualizar Control de Horas</h3>
+                <label for="txtControlHorasID">ID del Control de Horas:</label>
+                <input type="number" id="txtControlHorasID" runat="server" />
+
+                <label for="txtHoraSalida">Hora de Salida:</label>
+                <input type="time" id="txtHoraSalida" runat="server" />
+
+                <asp:Button ID="btnActualizarControlHoras" runat="server" Text="Actualizar Control de Horas" OnClick="btnActualizarControlHoras_Click" />
+            </div>
+
+            <!-- Formulario para eliminar un control de horas -->
+            <div class="form-section">
+                <h3>Eliminar Control de Horas</h3>
+                <label for="txtEliminarControlHorasID">ID del Control de Horas:</label>
+                <input type="number" id="txtEliminarControlHorasID" runat="server" />
+                <asp:Button ID="btnEliminarControlHoras" runat="server" Text="Eliminar Control de Horas" OnClick="btnEliminarControlHoras_Click" />
+            </div>
+
+                 <!-- Botón para redirigir a la gestión de empleados -->
+         <div class="form-section">
+             <asp:Button ID="btnPagGestionAusencias" runat="server" Text="Gestion de Ausencias" OnClick="btnPagGestionAusencias_Click" />
+         </div>
+    </form>
+</body>
+</html>
---
+++ HR Manager/Pages/PagControlHoras.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HR_Manager.Pages
+{
+    public partial class PagControlHoras : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Lógica de inicialización si es necesario
+        }
+
+        // Maneja la acción de agregar un control de horas
+        protected void btnAgregarControlHoras_Click(object sender, EventArgs e)
+        {
+            int empleadoID = Convert.ToInt32(txtEmpleadoID.Value);
+            DateTime fecha = Convert.ToDateTime(txtFecha.Value);
+            TimeSpan horaEntrada = TimeSpan.Parse(txtHoraEntrada.Value);
+
+            bool success = clControlHoras.AgregarControlHoras(empleadoID, fecha, horaEntrada);
+
+            if (success)
+            {
+                // Mensaje de éxito
+                Response.Write("<script>alert('Control de horas agregado exitosamente');</script>");
+            }
+            else
+            {
+                // Mensaje de error
+                Response.Write("<script>alert('Hubo un error al agregar el control de horas');</script>");
+            }
+        }
+
+        // Maneja la acción de actualizar un control de horas
+        protected void btnActualizarControlHoras_Click(object sender, EventArgs e)
+        {
+            int controlHorasID = Convert.ToInt32(txtControlHorasID.Value);
+            TimeSpan horaSalida = TimeSpan.Parse(txtHoraSalida.Value);
+
+            bool success = clControlHoras.ActualizarControlHoras(controlHorasID, horaSalida);
+
+            if (success)
+            {
+                // Mensaje de éxito
+                Response.Write("<script>alert('Control de horas actualizado exitosamente');</script>");
+            }
+            else
+            {
+                // Mensaje de error
+                Response.Write("<script>alert('Hubo un error al actualizar el control de horas');</script>");
+            }
+        }
+
+        // Maneja la acción de eliminar un control de horas
+        protected void btnEliminarControlHoras_Click(object sender, EventArgs e)
+        {
+            int controlHorasID = Convert.ToInt32(txtEliminarControlHorasID.Value);
+
+            bool success = clControlHoras.EliminarControlHoras(controlHorasID);
+
+            if (success)
+            {
+                // Mensaje de éxito
+                Response.Write("<script>alert('Control de horas eliminado exitosamente');</script>");
+            }
+            else
+            {
+                // Mensaje de error
+                Response.Write("<script>alert('Hubo un error al eliminar el control de horas');</script>");
+            }
+        }
+        // Redirigir a la página de gestión de estado laboral
+        protected void btnPagGestionAusencias_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("PagGestionAusenciass.aspx");
+        }
+
+        // Método para mostrar mensajes en la página
+        private void MostrarMensaje(string mensaje, string tipo)
+        {
+            // Puedes agregar un contenedor para mostrar el mensaje en la página.
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", $"alert('{mensaje}');", true);
+        }
+    }
+}
